a5cc92d8ee66c3fffdff2d51f379e1b2bbaa9a6b
Matt Briancon <mattbriancon@users.noreply.github.com>
2017-05-26 18:10:41 -0400

    setup.py tweaks

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ __version__ = '0.0.1'
 with codecs.open('README.rst', 'r', 'utf-8') as f:
     readme = f.read()
 
-# TODO license
 setuptools.setup(
     name='mbq.metrics',
     long_description=readme,
     version=__version__,
     url='https://github.com/managedbyq/mbq.metrics',
-    author='Managed by Q',
+    author='Managed by Q, Inc.',
     author_email='open-source@managedbyq.com',
-    maintainer='Managed by Q',
+    maintainer='Managed by Q, Inc.',
     maintainer_email='open-source@managedbyq.com',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
