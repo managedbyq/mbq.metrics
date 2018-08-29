259d79f5fe23a6fb18a6a428d24db7f1e3d5759a
Per-Andre Stromhaug <pstromhaug@managedbyq.com>
2018-08-29 14:45:54 -0400

    bumping datadog version from 0.16.0 to 0.22.0

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
     keywords='metrics monitoring statsd',
     packages=setuptools.find_packages(),
     install_requires=[
-        'datadog==0.16.0',  # pinned until 1.0.0
+        'datadog==0.22.0',  # pinned until 1.0.0
     ],
     zip_safe=True,
 )
