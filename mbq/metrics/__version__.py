70554f36e350892a4586272325ebe8e1be05ee79
Matt Briancon <mattbriancon@users.noreply.github.com>
2018-08-31 13:13:33 -0400

    Fixes

--- mbq/metrics/__version__.py
+++ mbq/metrics/__version__.py
@@ -1,7 +1,7 @@
 __author__ = 'Managed by Q, Inc.',
-__author_email__ = 'open-source@managedbyq.com',
+__author_email__ = 'open-source@managedbyq.com'
 __description__ = 'MBQ Metrics'
 __license__ = 'Apache 2.0'
 __title__ = 'mbq.metrics'
-__url__ = 'https://github.com/managedbyq/mbq.metrics',
-__version__ = '0.4.0'
+__url__ = 'https://github.com/managedbyq/mbq.metrics'
+__version__ = '0.4.1'
