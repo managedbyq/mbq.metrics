95977c9784351b83f11f1c61f6e1f04104214bae
Travis Thieman <travis.thieman@gmail.com>
2018-08-31 11:10:33 -0400

    Add local port range heuristic

--- mbq/metrics/contrib/django/middleware/connection_stats.py
+++ mbq/metrics/contrib/django/middleware/connection_stats.py
@@ -21,6 +21,7 @@ class ConnectionStatsMiddleware(object):
     This is based on the global /proc/net so it will give you inaccurate
     results if your application is not running in its own container.
     """
+    _local_port_range = None
 
     def __init__(self, get_response):
         self.get_response = get_response
@@ -37,11 +38,31 @@ class ConnectionStatsMiddleware(object):
         response = self.get_response(request)
         return response
 
+    @property
+    def local_port_range(self):
+        """Tuple of (low_port, high_port) reflecting the local port range
+        assigned to outbound connections. We use this as part of a heuristic
+        to determine whether a connection is inbound or outbound.
+        """
+        if self._local_port_range is None:
+            with open('/proc/sys/net/ipv4/ip_local_port_range', 'r') as f:
+                self._local_port_range = tuple(map(int, f.read().split('\t')))
+        return self._local_port_range
+
     def report_metrics(self):
         established_connections, waiting_connections = 0, 0
         with open('/proc/net/tcp', 'r') as f:
             for line in f.read().splitlines()[1:]:
-                state_int = int(line.strip().split(' ')[3], 16)
+                parts = line.strip().split(' ')
+                local_low, local_high = self.local_port_range
+                local_address, local_port = parts[1].split(':')
+
+                # If connection's local port is in the "local port" range,
+                # we treat it like an outgoing connection and skip it
+                if local_low <= int(local_port, 16) <= local_high:
+                    continue
+
+                state_int = int(parts[3], 16)
                 if state_int == ESTABLISHED_STATE:
                     established_connections += 1
                 elif state_int == SYN_RECV_STATE:
