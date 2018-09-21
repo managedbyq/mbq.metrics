95489907274a37afb598a57ef9171d05e86e9555
Per-Andre Stromhaug <pstromhaug@managedbyq.com>
2018-09-21 11:06:16 -0400

    remove trailing '/' from paths

--- mbq/metrics/contrib/utils.py
+++ mbq/metrics/contrib/utils.py
@@ -7,7 +7,9 @@ UUID_REGEX = re.compile(r'/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a
 
 def _sluggified_path(path):
     path = re.sub(UUID_REGEX, '/:id', path)
-    return re.sub(DIGIT_ID_REGEX, '/:id', path)
+    path = re.sub(DIGIT_ID_REGEX, '/:id', path)
+    path = path[:-1] if path[-1] == '/' else path  # remove trailing '/' at end of urls
+    return path
 
 
 def get_response_metrics_tags(status_code, path, method):
