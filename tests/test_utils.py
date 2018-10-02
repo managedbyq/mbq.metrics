a9b6c0b950301060ec2a76ab5be6966bcf90016d
Travis Thieman <travis.thieman@gmail.com>
2018-10-02 15:16:19 -0400

    Support lists, tuples, or dicts for tags (#24)
    * Support lists, tuples, or dicts for tags
    
    * Python 2 copy support

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,22 +3,34 @@ from unittest import TestCase
 from mbq.metrics import utils
 
 
-class TagDictToListTest(TestCase):
+class TagsAsListTest(TestCase):
     def test_key_and_value_changed_to_strings(self):
-        tags = utils.tag_dict_to_list({
+        tags = utils.tags_as_list({
             1: 2,
         })
         self.assertEqual(tags, ['1:2'])
 
     def test_allow_missing_value(self):
-        tags = utils.tag_dict_to_list({
+        tags = utils.tags_as_list({
             'test': None,
         })
         self.assertEqual(tags, ['test'])
 
     def test_allow_many_tags(self):
-        tags = utils.tag_dict_to_list({
+        tags = utils.tags_as_list({
             'test1': None,
             'test2': 'dogs',
         })
-        self.assertEqual(set(tags), {'test1', 'test2:dogs'})
+        self.assertEqual(tags, ['test1', 'test2:dogs'])
+
+    def test_input_list(self):
+        tags = utils.tags_as_list(['tag1:value1', 'tag2:value2'])
+        self.assertEqual(tags, ['tag1:value1', 'tag2:value2'])
+
+    def test_input_tuple(self):
+        tags = utils.tags_as_list(('tag1:value1', 'tag2:value2'))
+        self.assertEqual(tags, ['tag1:value1', 'tag2:value2'])
+
+    def test_input_none(self):
+        tags = utils.tags_as_list(None)
+        self.assertEqual(tags, [])
