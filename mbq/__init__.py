6b9cc8477eac8b487d7e79c8441a51bbaab755ba
Matt Briançon <mattbriancon@users.noreply.github.com>
2019-06-28 15:51:44 -0400

    more mypy (#36)
    * more mypy
    
    * lint on py36 and py37
    
    * MANIFEST.in
    
    * Type for __path__

--- mbq/__init__.py
+++ mbq/__init__.py
@@ -1,2 +1,6 @@
 # https://packaging.python.org/namespace_packages/#pkgutil-style-namespace-packages
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)  # type: ignore
+import typing
+
+
+__path__: typing.Iterable[str]  # https://docs.python.org/3/reference/import.html#__path__
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
